0f7d6169059baa974c56741680323b531f16db46
Klaus Rabbertz <klaus.rabbertz@cern.ch>
2008-04-22 14:39:30 +0000

    Updated Z mass, fixed double prec. problem, added own as and PDF as routine use. Needs some clean-up
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@313 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v1.4/user1c/fn-interface.f
+++ v1.4/user1c/fn-interface.f
@@ -27,48 +27,69 @@
 *
 *-----------------------------------------------------------------
       IMPLICIT NONE
-      INTEGER nloop,ifirst
-      DOUBLE PRECISION MUR, ALPSMZ, PI
-      DOUBLE PRECISION ALPS_IT 
-c      DOUBLE PRECISION MY_FAVOURITE-ALPHAS
-ckr 30.01.2008: Initialize pi in double precision at first call like elsewhere
-ckr      PARAMETER (PI=3.1415927d0)
-      DOUBLE PRECISION ZMASS, RALPSL, RALPSM, A2PI1, A2PI2
-      PARAMETER (ZMASS = 91.187D0)
+      DOUBLE PRECISION MUR
+      DOUBLE PRECISION ALPS_IT,ALPHASPDF,RALPSL,RALPSM
+      DOUBLE PRECISION ASMZ,ASMZPDF,A2PI1,A2PI2
+      INTEGER IOAS,NLOOP
+
+      DOUBLE PRECISION ZMASS
+ckr Old Z mass
+ckr      PARAMETER (ZMASS = 91.187D0)
+ckr Z mass from PDG 2006
+      PARAMETER (ZMASS = 91.1876D0)
+
+ckr PDG 2006
+      DOUBLE PRECISION ASMZPDG
+      PARAMETER (ASMZPDG = 0.1176D0)
+
+ckr For LHC energies assume always 5 flavours
       INTEGER NF
       PARAMETER (NF = 5)
 
+ckr 30.01.2008: Initialize pi in double precision at first call like elsewhere
+ckr      PARAMETER (PI=3.1415927D0)
+      INTEGER IFIRST
+      DOUBLE PRECISION PI
       DATA IFIRST,PI/0,0.D0/
       SAVE IFIRST,PI
       
+      call GetOrderAs(IOAS)
+      ASMZPDF = ALPHASPDF(ZMASS)
+ckr Round value to 6 digits only
+      ASMZPDF = ANINT(ASMZPDF*1D6)/1D6
       IF (IFIRST.EQ.0) THEN
          IFIRST = 1
          PI = 4D0 * ATAN(1D0)
+         WRITE(*,*)"FNALPHAS: PI =",PI 
+         WRITE(*,*)"FNALPHAS: M_Z/GeV =",ZMASS 
+         WRITE(*,*)"FNALPHAS: a_s(M_Z)_PDG (parameter) =",ASMZPDG 
+         WRITE(*,*)"FNALPHAS: a_s(M_Z)_PDF (1st call)  =",ASMZPDF 
+         WRITE(*,*)"FNALPHAS: a_s was used in",IOAS+1,
+     >        "-loop order in PDF"
       ENDIF
 
 c === example: exact, iterative solution of the 2-loop RGE 
-      nloop=2
-ckr 30.01.2008: Initialize alphas(M_Z) in double precision
-ckr      alpsmz=0.118              ! set here the value of alpha_s(Mz)
-ckr      alpsmz=0.118d0              ! set here the value of alpha_s(Mz)
-      alpsmz=0.125d0              ! set here the value of alpha_s(Mz)
-c      alpsmz=0.1185              ! for H1-2000 MSbar
-c      alpsmz=0.1205             ! for MRST2004
-ckr      FNALPHAS = ALPS_IT(MUR,ALPSMZ,NLOOP)/2d0/PI
-ckr      A2PI1 = ALPS_IT(MUR,ALPSMZ,NLOOP)/2d0/PI
+c      nloop=2
+c      ASMZ=0.118              ! set here the value of alpha_s(Mz)
+c      ASMZ=0.1185             ! for H1-2000 MSbar
+c      ASMZ=0.1205             ! for MRST2004
+ckr 30.01.2008: Initialize alphas(M_Z) in double precision!
+ckr      ASMZ=0.118D0              ! set here the value of alpha_s(Mz)
+ckr      FNALPHAS = ALPS_IT(MUR,ASMZ,NLOOP)/2d0/PI
+ckr      A2PI1 = ALPS_IT(MUR,ASMZ,NLOOP)/2d0/PI
 
-
-c - here you can call your own alpha_s code
+c === here you can call your own alpha_s code
 c           -> remember to divide by 2Pi
-c
-      nloop=1
-      alpsmz=0.25
-      FNALPHAS = RALPSL(MUR,ZMASS,ALPSMZ,NF,NLOOP)/2d0/PI
-ckr      FNALPHAS = RALPSM(MUR,ZMASS,ALPSMZ,NF,NLOOP)/2d0/PI
-ckr      A2PI2 = RALPSM(MUR,ZMASS,ALPSMZ,NF,NLOOP)/2d0/PI
+
+      NLOOP = IOAS+1
+      ASMZ = ASMZPDF
+ckr      ASMZ = ASMZPDG
+      FNALPHAS = ALPHASPDF(MUR)/2D0/PI
+ckr      FNALPHAS = RALPSL(MUR,ZMASS,ASMZ,NF,NLOOP)/2D0/PI
+ckr      FNALPHAS = RALPSM(MUR,ZMASS,ASMZ,NF,NLOOP)/2D0/PI
+ckr      A2PI2 = RALPSM(MUR,ZMASS,ASMZ,NF,NLOOP)/2D0/PI
 ckr      write(*,*)"d(A2PI) =",abs(a2pi2-a2pi1)/a2pi1
 ckr      FNALPHAS = A2PI2
-c
 
       RETURN
       END
@@ -99,6 +120,7 @@ c remember to initialize the LHAPDF set first (in the main routine)
 c                                       -> see the fastNLO example
 c             call InitPDFset("cteq61.LHgrid")
 c             call InitPDF(0)
+      write(*,*)"hallo x",x
       call evolvePDF(X,MUF,XPDF)
 c
 
