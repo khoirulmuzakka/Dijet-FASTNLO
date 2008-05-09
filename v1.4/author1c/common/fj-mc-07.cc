5f948f834a6432a1a1402b261da128faaa62a1d3
Klaus Rabbertz <klaus.rabbertz@cern.ch>
2008-05-09 17:07:08 +0000

    Commented out comments after bugfix in plugin call
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@339 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v1.4/author1c/common/fj-mc-07.cc
+++ v1.4/author1c/common/fj-mc-07.cc
@@ -1,5 +1,7 @@
 #include "fj-mc-07.h"
 #include "fastjet/CDFMidPointPlugin.hh"
+// Well, in SISCone the following header is already included at some place
+#include "fastjet/ClusterSequence.hh"
 #include <cmath>
 using namespace std;
 
@@ -18,11 +20,11 @@ fj_mc_07::operator()(const event_hhc& ev, double rwhatfor)
     const double py = ev[ip].Y();
     const double pz = ev[ip].Z();
     const double E  = ev[ip].T();
-    cout << "**************************\n";
-    cout << "Input objects: ip, px, py, pz, E, np\n";
-    printf("%5u %15.8f %15.8f %15.8f %15.8f %8u\n",
- 	   ip, px, py, pz, E, nt);
-    cout << "**************************\n";
+//     cout << "**************************\n";
+//     cout << "Input objects: ip, px, py, pz, E, np\n";
+//     printf("%5u %15.8f %15.8f %15.8f %15.8f %8u\n",
+//  	   ip, px, py, pz, E, nt);
+//     cout << "**************************\n";
     input_objects.push_back(fastjet::PseudoJet(px,py,pz,E));
   }
 
@@ -39,17 +41,12 @@ fj_mc_07::operator()(const event_hhc& ev, double rwhatfor)
   plugin = new fastjet::CDFMidPointPlugin (seed_threshold, cone_radius,
                                            cone_area_fraction, max_pair_size,
                                            max_iterations, overlap_threshold);
-
+  
   // create a jet-definition based on the plugin
   fastjet::Strategy strategy = fastjet::Best;
   fastjet::RecombinationScheme recomb_scheme = fastjet::E_scheme;
-
-  //    fastjet::JetDefinition jet_def(plugin);
-  fastjet::JetDefinition jet_def(plugin, cone_radius, recomb_scheme, strategy);
+  fastjet::JetDefinition jet_def(plugin);
   fastjet::ClusterSequence clust_seq(input_objects, jet_def);
-  
-
-
 
   double ptmin = 1.0;
   vector<fastjet::PseudoJet> inclusive_jets = clust_seq.inclusive_jets(ptmin);
@@ -66,39 +63,39 @@ fj_mc_07::operator()(const event_hhc& ev, double rwhatfor)
     _M_pj.push_back( _Lv(px, py, pz, E) );
   }
 
-  double Emax1 = 0.;
-  double Emax2 = 0.;
-  unsigned int iord[] = {0,0,0}; 
-  for (unsigned int j = 1; j <= nj; j++) {
-    double E  = _M_pj[j].T();
-    if ( E > Emax1 ) {
-      Emax2 = Emax1;
-      Emax1 = E;
-      iord[2] = iord[1];
-      iord[1] = iord[0];
-      iord[0] = j;
-    } else if ( E > Emax2 ) {
-      Emax2 = E;
-      iord[2] = iord[1];
-      iord[1] = j;
-    } else {
-      iord[2] = j;
-    }
-  }
-  for (unsigned int j = 1; j <= nj; j++) {
-   if ( iord[j-1] > 0 ) {
-      double px = _M_pj[iord[j-1]].X();
-      double py = _M_pj[iord[j-1]].Y();
-      double pz = _M_pj[iord[j-1]].Z();
-      double E  = _M_pj[iord[j-1]].T();
-      double pt = sqrt(px*px + py*py);
-      cout << "**************************" << endl;
-      cout << "Output jets pt >= 1.0:" << endl;
-      printf("%5u %15.8f %15.8f %15.8f %15.8f %15.8f\n",
-	     j, px, py, pz, E, pt);
-      cout << "**************************" << endl;
-    }
-  }
+//   double Emax1 = 0.;
+//   double Emax2 = 0.;
+//   unsigned int iord[] = {0,0,0}; 
+//   for (unsigned int j = 1; j <= nj; j++) {
+//     double E  = _M_pj[j].T();
+//     if ( E > Emax1 ) {
+//       Emax2 = Emax1;
+//       Emax1 = E;
+//       iord[2] = iord[1];
+//       iord[1] = iord[0];
+//       iord[0] = j;
+//     } else if ( E > Emax2 ) {
+//       Emax2 = E;
+//       iord[2] = iord[1];
+//       iord[1] = j;
+//     } else {
+//       iord[2] = j;
+//     }
+//   }
+//   for (unsigned int j = 1; j <= nj; j++) {
+//    if ( iord[j-1] > 0 ) {
+//       double px = _M_pj[iord[j-1]].X();
+//       double py = _M_pj[iord[j-1]].Y();
+//       double pz = _M_pj[iord[j-1]].Z();
+//       double E  = _M_pj[iord[j-1]].T();
+//       double pt = sqrt(px*px + py*py);
+//       cout << "**************************\n";
+//       cout << "Output jets pt >= 1.0: ijet, px, py, pz, E, pt\n";
+//       printf("%5u %15.8f %15.8f %15.8f %15.8f %15.8f\n",
+// 	     j, px, py, pz, E, pt);
+//       cout << "**************************\n";
+//     }
+//   }
 
   delete plugin;
   return _M_pj;
