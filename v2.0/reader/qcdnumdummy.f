b17720e430e16ff0c0b61e6c3b3879069dde00a7
Daniel Britzger <daniel.britzger@desy.de>
2011-08-18 20:47:36 +0000

    FastNLOReader. Initial commit. Can read DIS v2.0, and DIS MuVar tables. Ready for release.
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@825 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

---
+++ v2.0/reader/qcdnumdummy.f
@@ -0,0 +1,15 @@
+
+      subroutine fpdfxq(jset,xx,qq,pdf,jchk)
+c     subroutine fortfunc(ii,ff)
+c     integer ii
+c     real*4  ff
+      
+c     write(6,100) ii, ff
+c     100    format('ii=',i2,' ff=',f6.3)
+
+      return
+      end
+
+      subroutine evolution()
+      return
+      end
