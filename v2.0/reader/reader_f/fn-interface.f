36013cc67480a9a6e61a56315197cf1ab2c51e1f
Klaus Rabbertz <klaus.rabbertz@cern.ch>
2012-02-08 17:08:23 +0000

    v2: Bug fix: Float initialization of alpha_s(M_Z) fixed ==> eliminates O(10-6) differences to C++ reader; now O(10-11); clean-up
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1046 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/reader/reader_f/fn-interface.f
+++ v2.0/reader/reader_f/fn-interface.f
@@ -1,31 +1,41 @@
-******************************************************************
-* M. Wobisch - July 26, 2005           fn-interface.f
+***********************************************************************
+*     
+*     fastNLO user interface to PDF and alpha_s code
+*     
+*     Initial version: M. Wobisch, 2005
+*     Updated for v2:  K. Rabbertz, M. Wobisch, 2011
+*     
+*     Contains:    
+*     ---------
+*     DOUBLE PRECISION FUNCTION FNALPHAS(MUR)
+*     SUBROUTINE FNPDF(X,MUF,XPDF)
+*     
+*     By default:
+*     -----------
+*     - The PDF interface FNPDF is set up to access LHAPDF
+*     - The alpha_s routine FNALPHAS calls a calculation of
+*     > alpha_s in the MSbar scheme for given alpha_s(Mz)
+*     > using an exact, iterative solution of 2-/3-/4-loop
+*     > formulas as used by GRV hep-ph/9806404
 *
-*   fastNLO user interface to PDF and alpha_s code
-*   --> to be edited by user 
-*       to interface own PDF/alphas code
-*
-* included:    
-*      DOUBLE PRECISION FUNCTION FNALPHAS(MUR)
-*      SUBROUTINE FNPDF(X,MUF,XPDF)
-*
-*  in the default version the PDF interface FNPDF is set up
-*  to access LHAPDF - the alpha_s routine FNALPHAS calls
-*  alphas-demo.f which is an iterative solution of the 2-loop RGE
-*******************************************************************
-
+***********************************************************************
 
       DOUBLE PRECISION FUNCTION FNALPHAS(MUR)
-*-----------------------------------------------------------------
-* MW 06/29/2005  - alphas interface to the fastNLO code
+***********************************************************************
+*     
+*     Interface for alpha_s computation
+*     
+*     Input:
+*     ------
+*     MUR       renormalization scale in GeV
 *
-* alpha_s computation
-*   input:   MUR     renormalization scale in GeV
-*   output:  value of (alpha_s/2Pi) at scale MUR
-*
-*  !!!! again: this function must return  alpha_s/(2Pi)  !!!!!
-*
-*-----------------------------------------------------------------
+*     Output:
+*     -------
+*     FNALPHAS  value of (alpha_s/2Pi) at scale MUR
+*     
+*     ATTENTION: This function MUST return alpha_s/(2Pi) !
+*     
+***********************************************************************
       IMPLICIT NONE
       DOUBLE PRECISION MUR
       
@@ -41,60 +51,57 @@
          PI  = PI4/4D0
       ENDIF
 
-*     Example:
+*---  Example:
 *     Calculation of alpha_s in the MSbar scheme for given alpha_s(Mz)
-*     using exact, iterative solution of 2-/3-/4-loop formulas
+*     using an exact, iterative solution of 2-/3-/4-loop formulas
 *     as used by GRV hep-ph/9806404
       NLOOP  = 2
-      ALPSMZ = 0.1185           ! Bethke 2011 (Ref. ???)
-
-*     One can also call ones own alpha_s code here
-*     --> Only one has to remember to divide by 2Pi!
-ckr New code FNLO v2
+      ALPSMZ = 0.1185D0         ! Bethke 2011 (Ref. ???)
+      
+*---  One can also call ones own alpha_s code here ...
+*---  --> Only one has to remember to divide by 2Pi!
+C---  New code FNLO v2
       FNALPHAS = ALPS_IT(MUR,ALPSMZ,NLOOP)/2D0/PI
-ckr Old code FNLO v14
-ckr      FNALPHAS = ALPS_IT_FNLO14(MUR,ALPSMZ,NLOOP)/2D0/PI
-ckr Fix alpha_s for debugging
-ckr      FNALPHAS = ALPSMZ/2D0/PI
-
+C---  Old code FNLO v14
+C---  FNALPHAS = ALPS_IT_FNLO14(MUR,ALPSMZ,NLOOP)/2D0/PI
+C---  Fix alpha_s for debugging
+C---  FNALPHAS = ALPSMZ/2D0/PI
+      
       RETURN
       END
-
-C *****************************************************************
+***********************************************************************
 
       SUBROUTINE FNPDF(X,MUF,XPDF)
-*-----------------------------------------------------------------
-* MW 06/29/2005 
-*
-* PDF interface to the fastNLO usercode
-*
-*   input   X       parton momentum fraction 
-*           MUF     factorization scale in GeV
-*
-*   output  XPDF(-6:6) array of PDF momentum densities i.e. x*pdf!
-*                      using the LHAPDF numbering convention:
-*        tbar, bbar, cbar, sbar, ubar, dbar, g, d, u, s, c, b, t
-*         -6 ,  -5 ,  -4 ,  -3 ,  -2 ,  -1 , 0, 1, 2, 3, 4, 5, 6
-*
-*-----------------------------------------------------------------
+***********************************************************************
+*     
+*     PDF interface to the fastNLO usercode
+*     
+*     Input:
+*     ------
+*     X    parton momentum fraction 
+*     MUF  factorization scale in GeV
+*     
+*     Output:
+*     -------
+*     XPDF(-6:6)  array of PDF momentum densities i.e. x*pdf !
+*     >           using the LHAPDF numbering convention:
+*     >           tbar,bbar,cbar,sbar,ubar,dbar,g,d,u,s,c,b,t
+*     >            -6 , -5 , -4 , -3 , -2 , -1 ,0,1,2,3,4,5,6
+*     
+***********************************************************************
       IMPLICIT NONE
       DOUBLE PRECISION X, MUF, XPDF(-6:6)
+*---  Example:
+*     Interface to LHAPDF
+*     Remember to initialize the LHAPDF set first (in the main routine):
+*     >  CALL INITPDFSET("cteq61.LHgrid")
+*     >  CALL INITPDF(0)
+      CALL EVOLVEPDF(X,MUF,XPDF)
 
-
-c ======= example: interface to LHAPDF ============================
-c remember to initialize the LHAPDF set first (in the main routine)
-c                                       -> see the fastNLO example
-c             call InitPDFset("cteq61.LHgrid")
-c             call InitPDF(0)
-      call evolvePDF(X,MUF,XPDF)
-c
-
-
-c === here you can call your own PDF code
-c  -> remember that these are *momentum densities* i.e. x * PDF
-c     and the scale is in GeV
-c
-c     call MY-FAVORITE-PDFS(....)
-c
+*---  Here one can also call ones own PDF code
+*---  --> Only remember that these are MOMENTUM DENSITIES, i.e. x * PDF
+*---  --> and the scale is in GeV
+C---  CALL MY-FAVORITE-PDFS(....)
+      
       RETURN
       END
