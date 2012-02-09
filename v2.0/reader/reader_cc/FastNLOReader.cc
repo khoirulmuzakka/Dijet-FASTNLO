1d68f96b71fb82040401870a373e475bf64f1c02
Daniel Britzger <daniel.britzger@desy.de>
2012-02-09 10:42:13 +0000

    Make sure, that FastNLOReader::fAlphasMz is used for Alphas calculation.
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1052 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/reader/reader_cc/FastNLOReader.cc
+++ v2.0/reader/reader_cc/FastNLOReader.cc
@@ -1978,6 +1978,7 @@ double FastNLOReader::CalcAlphasNLOJET(double Q, double alphasMZ){
 
 
 double FastNLOReader::CalcAlphasGRV(double MU, double ALPSMZ){
+  Alphas::SetAlphasMz(fAlphasMz);
   return Alphas::CalcAlphasMu(MU,ALPSMZ);
 }
 
