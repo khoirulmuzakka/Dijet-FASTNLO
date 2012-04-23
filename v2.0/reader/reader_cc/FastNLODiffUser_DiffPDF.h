af61892db2b578aed4e6726c9e36899fca08685c
Daniel Britzger <daniel.britzger@desy.de>
2012-04-23 10:42:28 +0000

    User module for 'diffpdf' as used by H1.
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1117 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

---
+++ v2.0/reader/reader_cc/FastNLODiffUser_DiffPDF.h
@@ -0,0 +1,118 @@
+// Author: Daniel Britzger
+// DESY, 02/04/2012
+
+#ifndef FASTNLODIFFUSER
+#define FASTNLODIFFUSER
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//  FastNLODiffUSER                                                     //
+//                                                                      //
+//  FastNLODiffReader is a standalone code for reading                  //
+//  diffractive FastNLO tables of version 2.0 for DIS processes         //
+//                                                                      //
+//  FastNLO is developed by                                             //
+//    D. Britzger, T. Kluge, K. Rabbertz, F. Stober, M. Wobisch         //
+//    (publication in preparation)                                      //
+//    http://projects.hepforge.org/fastnlo                              //
+//                                                                      //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+
+#include <string>
+#include <cstdio>
+#include <vector>
+#include "FastNLODiffReader.h"
+
+using namespace std;
+
+class FastNLODiffUser : public FastNLODiffReader {
+
+public:
+   
+   FastNLODiffUser(string filename);
+   ~FastNLODiffUser(void);
+  
+protected:
+   
+   // inherited functions
+   double EvolveAlphas(double Q, double alphasMz ) const ;
+   void InitPDF();
+   vector<double> GetXFX(double xp, double muf) const ;
+
+};
+
+#endif
+
+
+//______________________________________________________________________________
+
+
+extern "C"{
+   void diffpdf_(double* xpom, double*  zpom, double*  Q2, double *pdfs);
+}
+
+
+
+//______________________________________________________________________________
+
+
+
+
+FastNLODiffUser::FastNLODiffUser(string filename) : FastNLODiffReader(filename)
+{
+
+}
+
+
+//______________________________________________________________________________
+
+
+double FastNLODiffUser::EvolveAlphas(double Q, double alphasMz ) const {
+   // --- fastNLO user:
+   // Implementation of Alpha_s evolution as function of the
+   // factorization scale [and alphas(Mz)].
+   //
+
+   // do not call me!
+   cout << "Do not call this alphas evolution!"<< endl;
+   return 0;
+}
+
+
+//______________________________________________________________________________
+
+
+void FastNLODiffUser::InitPDF(){
+   // --- fastNLO user:
+   //  Initalize PDF parameters if necessary
+   //
+
+   // nothing todo!
+}
+
+
+//______________________________________________________________________________
+
+
+
+vector<double> FastNLODiffUser::GetXFX(double xp, double muf) const {
+   //
+   //  GetXFX is used to get the parton array from the
+   //  pdf-interface. It should return a vector of 13
+   //  parton flavors from tbar to t at a certain
+   //  x-proton and factorisation scale.
+   //
+   vector < double > a(13);
+   double zpom = xp/fxpom;
+   double xpo = fxpom;
+   if ( zpom > fzmin && zpom < fzmax ) {
+      diffpdf_(&xpo,&zpom,&muf,&a[0]);
+   }
+   return a;
+}
+
+
+//______________________________________________________________________________
