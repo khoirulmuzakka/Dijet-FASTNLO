834a90cf6bce699e89c09b34787c7d575289faaf
Daniel Britzger <daniel.britzger@desy.de>
2014-01-11 14:57:12 +0000

    1. merger, 2. playground
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1580 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

---
+++ v2.0/toolkit/src/fastNLO.cc
@@ -0,0 +1,52 @@
+//********************************************************************
+//
+//     fastNLO
+//     This is your playgroud to calculate cross sections
+//     with fastNLO.
+//
+//********************************************************************
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "fastnlotk/fastNLOLHAPDF.h"
+
+
+//______________________________________________________________________________________________________________
+int main(int argc, char** argv) { 
+   // usage: fastNLO <fastNLO-table.tab> [<LHAPDFfile>]
+   using namespace std;
+   using namespace fastNLO;      // namespace for fastNLO constants
+
+   // ---  Parse commmand line
+   if (argc <= 1) {
+      cout<<" Program fastNLO\n   usage:\n   fastNLO <fastNLO-table.tab> [<LHAPDF-file>]"<<endl;
+      exit(1);
+   }
+   // --- fastNLO table
+   string tablename =  (const char*) argv[1];   
+   //---  PDF set
+   string PDFFile = "MSTW2008lo68cl.LHgrid";
+   if (argc > 2)    PDFFile = (const char*) argv[2];
+
+
+   //--- give some output
+   cout<<" fastNLO: Evaluating table: " << tablename << endl;
+   cout<<" fastNLO: Using PDF set   : " << PDFFile << endl;
+
+
+   // --- this is your playgroud to use fastNLO
+   //  Calculate cross setions and/or test some options
+   //  For a documentation and function calls, please see
+   //    './src/fnlo-cppread.cc'
+
+   //--- example calculation
+   fastNLOLHAPDF fnlo(tablename,"cteq6m.LHpdf",0);
+   fnlo.CalcCrossSection();
+   fnlo.PrintCrossSections();
+   fnlo.PrintCrossSectionsDefault();
+
+   return 0;
+}
+
---
+++ v2.0/toolkit/src/fnlo-merge.cc
@@ -0,0 +1,77 @@
+//********************************************************************
+//
+//     fnlo-merge
+//     Program to merge several fastNLO files with different or
+//     identical contributions into one table.
+//
+//********************************************************************
+// DB, 13.11.13, (re)write fnlo-merge for the toolkit 
+
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include "fastnlotk/speaker.h"
+#include "fastnlotk/fastNLOTable.h"
+
+using namespace std;
+using namespace say;
+
+int main(int argc, char** argv) 
+{
+   // ----------------------------------------------------------------------//
+   // Program to merge fastNLO tables.
+   // Input can be LO, NLO or other contributions (non-pert., data, etc)
+   // for the identical scenario.
+   // ----------------------------------------------------------------------//
+   
+   // check parameters
+   if (argc < 3) {
+      error["fnlo-merge"]<<"Usage: fnlo-merge file1.txt [filex.txt]+ result.txt"<<endl;
+      return 1;
+   }
+   
+   // check if output file already exists
+   int nFiles = argc - 1;
+   if (access(argv[nFiles], R_OK) == 0) {
+      error["fnlo-merge"]<<"Error: Output file " << argv[nFiles] << " already exists!" << endl;
+      return 1;
+   }
+
+   // init output table
+   fastNLOTable* resultTable = NULL;
+
+   // loop over arguments and check existence of files
+   int nValidTables = 1;
+   for (int idxFile=0; idxFile<nFiles-1; idxFile++) {
+      string path = argv[idxFile+1];
+      // File there?
+      if (access(path.c_str(), R_OK) != 0) {
+	 error["fnlo-merge"]<<"Unable to access file. Skipping "<<path<<endl;
+      }
+      else { //ok, file exists
+	 // reading table
+	 fastNLOTable tab(path);
+	 //! Todo: check validity of table, here!
+	 if ( !resultTable )  
+	    resultTable = new fastNLOTable(tab);
+	 else { // adding table to result table 
+	    // check if 'scenario' is compatible
+	    if ( !resultTable->IsCompatible(tab) ) 
+	       warn["fnlo-merge"]<<"Table '"<<path<<"' is not compatible with initial table '"<<resultTable->GetFilename()<<"'. Skipping table."<<endl;
+	    else { // adding tables
+	       resultTable->AddTable(tab);
+	       nValidTables++;
+	    }
+	 }
+      }
+   }
+   info["fnlo-merge"]<<"Found "<<nValidTables<<" table file(s)."<<endl;
+   //if (nValidTables == 1) exit(1);
+
+   // Write result
+   string outfile = argv[nFiles];
+   resultTable->SetFilename(outfile);
+   info["fnlo-merge"]<<"Write merged results to file "<<resultTable->GetFilename()<<"."<<endl;
+   resultTable->WriteTable();
+   return 0;
+}
