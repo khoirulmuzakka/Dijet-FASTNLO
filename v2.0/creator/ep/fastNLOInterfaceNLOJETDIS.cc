dffe38500fac30ee99cbc168286bf57628d41ccb
Klaus Rabbertz <klaus.rabbertz@cern.ch>
2014-01-17 16:01:09 +0000

    Small corrections
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1611 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/creator/ep/fastNLOInterfaceNLOJETDIS.cc
+++ v2.0/creator/ep/fastNLOInterfaceNLOJETDIS.cc
@@ -1,6 +1,6 @@
 //
 // UsefulNlojetTools
-// 
+//
 // Collection of useful functions for the fastNLO interface
 // to NLOJET++.
 //
@@ -8,7 +8,7 @@
 #include <cstdio>
 #include <algorithm> //c++98
 #include <utility>   //c++11
-#include <fastnlotk/fastNLOCreate.h>
+#include "fastnlotk/fastNLOCreate.h"
 
 namespace UsefulNlojetTools {
 
@@ -19,7 +19,7 @@ namespace UsefulNlojetTools {
       unsigned int nj = 0, nu = 0 ,nd = 0;
       inputfunc(nj,nu,nd);
       return nj;
-   }  
+   }
 
    double GetEcms(){
       // get center of mass energy
@@ -36,34 +36,34 @@ namespace UsefulNlojetTools {
       unsigned int nj = 0, nu = 0 ,nd = 0;
       inputfunc(nj,nu,nd);
       return nj - 1;
-   }  
-   
+   }
+
    //_______________________________________________________________________
-   unsigned int GetOrderOfRun(const std::basic_string<char>& __file_name){ 
+   unsigned int GetOrderOfRun(const std::basic_string<char>& __file_name){
       // --- determine whether we are running LO or NLO
       // and add order of expansion to leading-order .
-      const char* const file = __file_name.c_str(); 
+      const char* const file = __file_name.c_str();
       static int ord = -1;
-      
+
       if ( ord == -1 ) {
-	 if(strstr(file,"born")!=NULL){
-	    //IsNLO = false;
-	    printf("\nfastNLO: This is a LO run!\n");
-	    ord = GetLoOrder();
-	 }else{
-	    if(strstr(file,"nlo")!=NULL){
-	       //IsNLO = true;
-	       printf("\nfastNLO: This is a NLO run!\n");
-	       ord = GetLoOrder();
-	       ord += 1;
-	    }else{
-	       // it is 'nlojet'
-	       printf("fastNLO: ERROR! This module can only be run at Born level or at NLO.\n");
-	       exit(1);
-	    }
-	 }
-	 cout<<"\nfastNLO: Found order of calculation to be: ord="<<ord<<endl;
-	 return ord;
+         if(strstr(file,"born")!=NULL){
+            //IsNLO = false;
+            printf("\nfastNLO: This is a LO run!\n");
+            ord = GetLoOrder();
+         }else{
+            if(strstr(file,"nlo")!=NULL){
+               //IsNLO = true;
+               printf("\nfastNLO: This is a NLO run!\n");
+               ord = GetLoOrder();
+               ord += 1;
+            }else{
+               // it is 'nlojet'
+               printf("fastNLO: ERROR! This module can only be run at Born level or at NLO.\n");
+               exit(1);
+            }
+         }
+         cout<<"\nfastNLO: Found order of calculation to be: ord="<<ord<<endl;
+         return ord;
       }
       else return ord;
    }
@@ -72,10 +72,10 @@ namespace UsefulNlojetTools {
    unsigned int GetNSubproc() {
       static int nSubproc = -1;
       if ( nSubproc == -1 ) {
-	 int nord = GetOrderOfRun("bla");
-	 int loord = GetLoOrder() ;
-	 if ( nord == loord ) nSubproc = 2 ;
-	 else nSubproc = 3;
+         int nord = GetOrderOfRun("bla");
+         int loord = GetLoOrder() ;
+         if ( nord == loord ) nSubproc = 2 ;
+         else nSubproc = 3;
       }
       return nSubproc;
    }
@@ -84,7 +84,7 @@ namespace UsefulNlojetTools {
    vector<fnloEvent> GetFlexibleScaleNlojetContribDIS(const event_dis& p , const amplitude_dis& amp , nlo::pdf_and_coupling_dis& dummypdf, double alem){
       const int nSubproc = GetNSubproc() ; // nlojet must have 3 subprocesses.
       static const double dummyMu2 = 100.*100.;
-      
+
       // make events
       vector<fnloEvent> ev(nSubproc);
 
@@ -92,11 +92,11 @@ namespace UsefulNlojetTools {
       double x = (p[-1]*p[0])/(p[-1]*p[hadron(0)]);
       //double Q2 = -((p[-1] - p[-2]).mag2());
       for ( int p = 0 ; p<nSubproc ; p++ )  {
-	 ev[p].SetProcessId( p );
-	 ev[p].SetX1( x );
+         ev[p].SetProcessId( p );
+         ev[p].SetX1( x );
       }
 
-      
+
       double coef = 389385730.*alem*alem;
 
       // ---- calcualte weights for fini contributions ---- //
@@ -109,42 +109,42 @@ namespace UsefulNlojetTools {
       wt[2] = pdfsigma;
       //wt *= 389385730.;
       //if(IXsectUnits!=12)  wt *= pow(10.,(IXsectUnits-12)) ;
-      
+
       nlo::weight_dis cPDF = dummypdf.pdf(x,dummyMu2*dummyMu2,2,3); // M1*M1 is a dummy scale
       nlo::amplitude_dis::contrib_type itype = amp.contrib();
 
       // weights
       double weights[5][nSubproc]; // weights[amp_i][proc]
       if ( nSubproc > 2 ) {
-	 for ( int kk = 0 ; kk<5 ; kk ++ )	 for ( int p = 0 ; p<nSubproc ; p ++ ) weights[kk][p] = 0; 
-	 if(itype == nlo::amplitude_dis::fini) {
-	    for ( int kk = 0 ; kk<5 ; kk ++ ){
-	       double wSigma = 1.0/3.0*(-amp._M_fini.amp[kk][1]*coef*cPDF[1] + 4.*amp._M_fini.amp[kk][2]*coef*cPDF[2]);
-	       double wDelta = 3.0*(amp._M_fini.amp[kk][1]*coef*cPDF[1] - amp._M_fini.amp[kk][2]*coef*cPDF[2]);
-	       double wGluon = amp._M_fini.amp[kk][0]*coef*cPDF[0];
-	       weights[kk][0] = wDelta;
-	       weights[kk][1] = wGluon;
-	       weights[kk][2] = wSigma;
-	    }
-	 }
+         for ( int kk = 0 ; kk<5 ; kk ++ )       for ( int p = 0 ; p<nSubproc ; p ++ ) weights[kk][p] = 0;
+         if(itype == nlo::amplitude_dis::fini) {
+            for ( int kk = 0 ; kk<5 ; kk ++ ){
+               double wSigma = 1.0/3.0*(-amp._M_fini.amp[kk][1]*coef*cPDF[1] + 4.*amp._M_fini.amp[kk][2]*coef*cPDF[2]);
+               double wDelta = 3.0*(amp._M_fini.amp[kk][1]*coef*cPDF[1] - amp._M_fini.amp[kk][2]*coef*cPDF[2]);
+               double wGluon = amp._M_fini.amp[kk][0]*coef*cPDF[0];
+               weights[kk][0] = wDelta;
+               weights[kk][1] = wGluon;
+               weights[kk][2] = wSigma;
+            }
+         }
       }
-      
+
       for(int p=0 ; p<nSubproc ; p++){
-	 // decompose nlojet-event
-	 if(itype == nlo::amplitude_dis::fini) {
-	    if (amp._M_fini.mode==0) { //finix1
-	       ev[p].SetWeight_MuIndependent( weights[0][p] );
- 	       ev[p].SetWeight_log_muf( weights[2][p] );		  
-	    }
-	    else if (amp._M_fini.mode==1) { //finix2
- 	       ev[p].SetWeight_MuIndependent( weights[1][p] );
- 	       ev[p].SetWeight_log_muf( weights[3][p] );		  
- 	       ev[p].SetWeight_log_mur( weights[4][p] );		  
-	    }
-	 }
-	 else { // no fini contribution
-	    ev[p].AddWeight_MuIndependent( wt[p] );
-	 }
+         // decompose nlojet-event
+         if(itype == nlo::amplitude_dis::fini) {
+            if (amp._M_fini.mode==0) { //finix1
+               ev[p].SetWeight_MuIndependent( weights[0][p] );
+               ev[p].SetWeight_log_muf( weights[2][p] );
+            }
+            else if (amp._M_fini.mode==1) { //finix2
+               ev[p].SetWeight_MuIndependent( weights[1][p] );
+               ev[p].SetWeight_log_muf( weights[3][p] );
+               ev[p].SetWeight_log_mur( weights[4][p] );
+            }
+         }
+         else { // no fini contribution
+            ev[p].AddWeight_MuIndependent( wt[p] );
+         }
       }
 
       return ev;
--- v2.0/creator/hadron/fastNLOInterfaceToNLOJET.cc
+++ v2.0/creator/hadron/fastNLOInterfaceToNLOJET.cc
@@ -1,6 +1,6 @@
 //
 // UsefulNlojetTools
-// 
+//
 // Collection of useful functions for the fastNLO interface
 // to NLOJET++.
 //
@@ -8,13 +8,13 @@
 #include <cstdio>
 #include <algorithm> //c++98
 #include <utility>   //c++11
-#include <fastnlotk/fastNLOCreate.h>
+#include "fastnlotk/fastNLOCreate.h"
 #include "pdf-hhc-dummy.h"
 
 namespace UsefulNlojetTools {
 
    //_______________________________________________________________________
-   // 
+   //
    pdf_hhc_dummy dummypdf;
 
    //_______________________________________________________________________
@@ -24,7 +24,7 @@ namespace UsefulNlojetTools {
       unsigned int nj = 0, nu = 0 ,nd = 0;
       inputfunc(nj,nu,nd);
       return nj;
-   }  
+   }
 
    double GetEcms(){
       // get center of mass energy
@@ -32,39 +32,39 @@ namespace UsefulNlojetTools {
       psinput(NULL,ecms);
       return sqrt(ecms);
    }
-   
+
    //_______________________________________________________________________
    unsigned int GetLoOrder(){
       return GetNj();
-   }  
+   }
 
    //_______________________________________________________________________
-   unsigned int GetOrderOfRun(const std::basic_string<char>& __file_name){ 
+   unsigned int GetOrderOfRun(const std::basic_string<char>& __file_name){
       // --- determine whether we are running LO or NLO
       // and add order of expansion to leading-order .
       static int ord = -1;
-      
+
       if ( ord == -1 ) {
-	 const char* const file = __file_name.c_str(); 
-	 if(strstr(file,"born")!=NULL){
-	    //IsNLO = false;
-	    printf("fastNLO: This is a LO run!\n");
-	    ord = GetNj();
-	    return ord;
-	 }else{
-	    if(strstr(file,"nlo")!=NULL){
-	       //IsNLO = true;
-	       printf("fastNLO: This is a NLO run!\n");
-	       ord = GetNj() + 1;
-	       return ord;
-	    }else{
-	       // it is 'nlojet'
-	       printf("fastNLO: ERROR! This module can only be run at Born level or at NLO.\n");
-	       exit(1);
-	    }
-	 }
-	 cout<<"\nfastNLO: Found order of calculation to be: ord="<<ord<<endl;
-	 return ord;
+         const char* const file = __file_name.c_str();
+         if(strstr(file,"born")!=NULL){
+            //IsNLO = false;
+            printf("fastNLO: This is a LO run!\n");
+            ord = GetNj();
+            return ord;
+         }else{
+            if(strstr(file,"nlo")!=NULL){
+               //IsNLO = true;
+               printf("fastNLO: This is a NLO run!\n");
+               ord = GetNj() + 1;
+               return ord;
+            }else{
+               // it is 'nlojet'
+               printf("fastNLO: ERROR! This module can only be run at Born level or at NLO.\n");
+               exit(1);
+            }
+         }
+         cout<<"\nfastNLO: Found order of calculation to be: ord="<<ord<<endl;
+         return ord;
       }
       else return ord;
    }
@@ -94,7 +94,7 @@ namespace UsefulNlojetTools {
    vector<fnloEvent> GetFlexibleScaleNlojetContribHHC(const event_hhc& p , const amplitude_hhc& amp ){
       static const int nSubproc = 7 ; // nlojet must have 7 subprocesses.
       static const double dummyMu2 = 91.*91.;
-      
+
       // make events
       vector<fnloEvent> ev(nSubproc);
 
@@ -102,25 +102,25 @@ namespace UsefulNlojetTools {
       double x1 = p[-1].Z()/p[hadron(-1)].Z();
       double x2 = p[0].Z()/p[hadron(0)].Z();
       for ( int p = 0 ; p<nSubproc ; p++ )  {
-	 ev[p].SetProcessId( p );
-	 ev[p].SetX1( x1 );
-	 ev[p].SetX2( x2 );
+         ev[p].SetProcessId( p );
+         ev[p].SetX1( x1 );
+         ev[p].SetX2( x2 );
       }
 
       // weights
       double weights[7][7]; // weights[amp_i][proc]
-      for ( int kk = 0 ; kk<7 ; kk ++ )	 for ( int p = 0 ; p<nSubproc ; p ++ ) weights[kk][p] = 0; 
-      
+      for ( int kk = 0 ; kk<7 ; kk ++ )  for ( int p = 0 ; p<nSubproc ; p ++ ) weights[kk][p] = 0;
+
       // access perturbative coefficients
       nlo::amplitude_hhc::contrib_type itype = amp.contrib();
       nlo::weight_hhc cPDF = dummypdf.pdf(x1,x2,dummyMu2,2,3); // 1/x1/x2
       static const double coef = 389385730.;
 
       for ( int kk = 0 ; kk<7 ; kk ++ ) {
-	 for ( int fid = 0 ; fid<nSubproc ; fid ++ ) {
-	    int nid = FastnloIdToNlojetIdHHC(fid);
-	    weights[kk][fid] = amp._M_fini.amp[kk][nid]*coef*cPDF[nid];
-	 }
+         for ( int fid = 0 ; fid<nSubproc ; fid ++ ) {
+            int nid = FastnloIdToNlojetIdHHC(fid);
+            weights[kk][fid] = amp._M_fini.amp[kk][nid]*coef*cPDF[nid];
+         }
       }
 
       // todo: calculate wt and wtorg from 'weights'
@@ -128,38 +128,38 @@ namespace UsefulNlojetTools {
       nlo::weight_hhc wt = wtorg;
       // - rearrange subprocesses
       for ( int fid = 0 ; fid<7 ; fid ++ ) {
-	 int nid = FastnloIdToNlojetIdHHC(fid);
-	 wt[fid] = wtorg[nid];
+         int nid = FastnloIdToNlojetIdHHC(fid);
+         wt[fid] = wtorg[nid];
       }
       wt *= coef;
-   
+
       if(x2>x1){
-	 // swap subprocesses 6,7
-	 swap(wt[5],wt[6]);
-	 for ( int kk = 0 ; kk<7 ; kk ++ )
-	    swap(weights[kk][5],weights[kk][6]);
-      }	       
+         // swap subprocesses 6,7
+         swap(wt[5],wt[6]);
+         for ( int kk = 0 ; kk<7 ; kk ++ )
+            swap(weights[kk][5],weights[kk][6]);
+      }
 
       for(int p=0 ; p<nSubproc ; p++){
-	 // decompose nlojet-event
-	 if(itype == nlo::amplitude_hhc::fini) {
-	    if (amp._M_fini.mode==0) { //finix1
-	       ev[p].SetWeight_MuIndependent( weights[0][p] );
-	       ev[p].SetWeight_log_muf( weights[3][p] );		  
-	    }
-	    else if (amp._M_fini.mode==1) { //finix2
-	       ev[p].SetWeight_MuIndependent( weights[1][p] );
-	       ev[p].SetWeight_log_muf( weights[4][p] );		  
-	    }
-	    else if(amp._M_fini.mode==2){ //fini1
-	       ev[p].SetWeight_MuIndependent( weights[2][p] );
-	       ev[p].SetWeight_log_muf( weights[5][p] );		  
-	       ev[p].SetWeight_log_mur( weights[6][p] );		  
-	    }
-	 }
-	 else { // no fini contribution
-	    ev[p].AddWeight_MuIndependent( wt[p] );
-	 }
+         // decompose nlojet-event
+         if(itype == nlo::amplitude_hhc::fini) {
+            if (amp._M_fini.mode==0) { //finix1
+               ev[p].SetWeight_MuIndependent( weights[0][p] );
+               ev[p].SetWeight_log_muf( weights[3][p] );
+            }
+            else if (amp._M_fini.mode==1) { //finix2
+               ev[p].SetWeight_MuIndependent( weights[1][p] );
+               ev[p].SetWeight_log_muf( weights[4][p] );
+            }
+            else if(amp._M_fini.mode==2){ //fini1
+               ev[p].SetWeight_MuIndependent( weights[2][p] );
+               ev[p].SetWeight_log_muf( weights[5][p] );
+               ev[p].SetWeight_log_mur( weights[6][p] );
+            }
+         }
+         else { // no fini contribution
+            ev[p].AddWeight_MuIndependent( wt[p] );
+         }
       }
 
       return ev;
@@ -169,10 +169,10 @@ namespace UsefulNlojetTools {
    unsigned int GetNSubproc() {
       static int nSubproc = -1;
       if ( nSubproc == -1 ) {
-	 int nord = GetOrderOfRun("bla");
-	 int loord = GetLoOrder() ;
-	 if ( nord == loord ) nSubproc = 6 ;
-	 else nSubproc = 7;
+         int nord = GetOrderOfRun("bla");
+         int loord = GetLoOrder() ;
+         if ( nord == loord ) nSubproc = 6 ;
+         else nSubproc = 7;
       }
       return nSubproc;
    }
@@ -182,7 +182,7 @@ namespace UsefulNlojetTools {
    vector<fnloEvent> GetFixedScaleNlojetContribHHC(const event_hhc& p , const amplitude_hhc& amp, double mu ){
       const int nSubproc = GetNSubproc() ;
       const double Mu2 = mu*mu;
-      
+
       // make events
       vector<fnloEvent> ev(nSubproc);
 
@@ -190,9 +190,9 @@ namespace UsefulNlojetTools {
       double x1 = p[-1].Z()/p[hadron(-1)].Z();
       double x2 = p[0].Z()/p[hadron(0)].Z();
       for ( int p = 0 ; p<nSubproc ; p++ )  {
-	 ev[p].SetProcessId( p );
-	 ev[p].SetX1( x1 );
-	 ev[p].SetX2( x2 );
+         ev[p].SetProcessId( p );
+         ev[p].SetX1( x1 );
+         ev[p].SetX2( x2 );
       }
 
       // weights
@@ -204,22 +204,22 @@ namespace UsefulNlojetTools {
       nlo::weight_hhc wt = wtorg;
       // - rearrange subprocesses
       for ( int fid = 0 ; fid<7 ; fid ++ ) {
-	 int nid = FastnloIdToNlojetIdHHC(fid);
-	 wt[fid] = wtorg[nid];
+         int nid = FastnloIdToNlojetIdHHC(fid);
+         wt[fid] = wtorg[nid];
       }
       wt *= coef;
-   
+
       if(x2>x1){
-	 // swap subprocesses 6,7
-	 swap(wt[5],wt[6]);
-      }	       
+         // swap subprocesses 6,7
+         swap(wt[5],wt[6]);
+      }
       if (nSubproc==6 ) {
-	 wt[5] = (wt[5]+wt[6])/2;
-	 wt[6] = wt[5];
+         wt[5] = (wt[5]+wt[6])/2;
+         wt[6] = wt[5];
       }
 
       for(int p=0 ; p<nSubproc ; p++){
-	 ev[p].AddWeight_MuIndependent( wt[p] );
+         ev[p].AddWeight_MuIndependent( wt[p] );
       }
       return ev;
    }
@@ -231,8 +231,8 @@ namespace UsefulNlojetTools {
       // return value can directly passed to fastNLOCreate
       vector<vector<fnloEvent> > ctrbs(scalevar.size());
       for ( unsigned int is = 0 ; is<scalevar.size() ; is++ ) {
-	 double smu = mu * scalevar[is];
-	 ctrbs[is] = GetFixedScaleNlojetContribHHC(p,amp,smu);
+         double smu = mu * scalevar[is];
+         ctrbs[is] = GetFixedScaleNlojetContribHHC(p,amp,smu);
       }
       return ctrbs;
    }
