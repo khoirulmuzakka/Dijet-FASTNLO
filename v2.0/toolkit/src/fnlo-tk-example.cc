364d9dd944208398c83d8e87525ebdf812295a87
Daniel Britzger <daniel.britzger@desy.de>
2014-02-12 18:58:58 +0000

    update example
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1677 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/toolkit/src/fnlo-tk-example.cc
+++ v2.0/toolkit/src/fnlo-tk-example.cc
@@ -12,6 +12,7 @@
 #include <vector>
 #include "fastnlotk/fastNLOLHAPDF.h"
 
+double Function_Mu(double s1, double s2);
 
 //______________________________________________________________________________________________________________
 int main(int argc, char** argv) {
@@ -21,15 +22,16 @@ int main(int argc, char** argv) {
 
    // ---  Parse commmand line
    if (argc <= 1) {
-      cout<<" Program fastNLO\n   usage:\n   fastNLO <fastNLO-table.tab> [<LHAPDF-file>]"<<endl;
-      exit(1);
+      cout<<" Program fastNLO"<<endl;
+      cout<<"   usage:"<<endl;
+      cout<<"   fastNLO <fastNLO-table.tab> [<LHAPDF-file>]"<<endl;
+      return 0;
    }
    // --- fastNLO table
    string tablename =  (const char*) argv[1];
    //---  PDF set
-   string PDFFile = "MSTW2008lo68cl.LHgrid";
+   string PDFFile = "CT10nnlo.LHgrid";
    if (argc > 2)    PDFFile = (const char*) argv[2];
-
 
    //--- give some output
    cout<<" fastNLO: Evaluating table: " << tablename << endl;
@@ -42,10 +44,32 @@ int main(int argc, char** argv) {
    //    './src/fnlo-cppread.cc'
 
    //--- example calculation
-   fastNLOLHAPDF fnlo(tablename,"cteq6m.LHpdf",0);
+   fastNLOLHAPDF fnlo(tablename,PDFFile,0);
+   fnlo.PrintFastNLOTableConstants();		// print even more information
+   fnlo.PrintTableInfo();			// print some valuable information
+   fnlo.SetContributionON(fastNLO::kFixedOrder,0,true); // switch contributions on/off. By default LO and NLO.
+   fnlo.SetContributionON(fastNLO::kFixedOrder,1,true);
+   fnlo.SetContributionON(fastNLO::kFixedOrder,2,false); // NNLO must be switched on explicitly
+   fnlo.SetExternalFuncForMuR( &Function_Mu );	// set function to calculate renormalization scale. See manual for default settings.
+   fnlo.SetExternalFuncForMuF( &Function_Mu );
    fnlo.CalcCrossSection();
    fnlo.PrintCrossSections();
-   fnlo.PrintCrossSectionsDefault();
-
+   //fnlo.PrintCrossSectionsDefault();
+   vector<double > xs = fnlo.GetCrossSection();
    return 0;
+
 }
+
+
+double Function_Mu(double s1, double s2) {
+   // --- fastNLO user: This is an example function
+   //     to demonstrate how you might perform the
+   //     definition of the scales using a
+   //     'flexible-scale'-table, where a function
+   //     of s1 and s2 can be used.
+   //     Which variables s1 and s2 stand for are
+   //     coded in the fastNLO table.
+   double mu = 173.;
+   return mu;
+}
+
