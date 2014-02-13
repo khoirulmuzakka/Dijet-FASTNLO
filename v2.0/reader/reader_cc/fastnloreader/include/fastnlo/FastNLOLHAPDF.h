5ebf86262df31a17f86546f05bf31f076b80830f
Daniel Britzger <daniel.britzger@desy.de>
2014-02-13 12:24:08 +0000

    bugfix when reading an new lhapdffile
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1683 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/reader/reader_cc/fastnloreader/include/fastnlo/FastNLOLHAPDF.h
+++ v2.0/reader/reader_cc/fastnloreader/include/fastnlo/FastNLOLHAPDF.h
@@ -200,12 +200,12 @@ vector<double> FastNLOLHAPDF::GetXFX(double xp, double muf) const {
 
 void FastNLOLHAPDF::SetLHAPDFFilename(string filename) {
 
+   if (filename != fLHAPDFFilename) fchksum = 0;
    fLHAPDFFilename = filename;
    #if defined LHAPDF_MAJOR_VERSION && LHAPDF_MAJOR_VERSION == 6
    PDFSet = new LHAPDF::PDFSet(filename);
    fnPDFs = PDFSet->size();
    #else
-   if (filename != fLHAPDFFilename) fchksum = 0;
    // Reset pdfset member to zero
    fiPDFMember = 0;
    // KR: Reactivated this. Why was it switched off?
