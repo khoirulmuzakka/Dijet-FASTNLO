d1397d8b16cb5b94c19619bfa43f62daeb508fb5
Georg Sieber <georg.sieber@cern.ch>
2014-02-19 22:23:14 +0000

    splitted fastNLOAlphas
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1707 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/toolkit/fastnlotoolkit/include/fastnlotk/fastNLOAlphas.h
+++ v2.0/toolkit/fastnlotoolkit/include/fastnlotk/fastNLOAlphas.h
@@ -40,15 +40,11 @@
 
 using namespace std;
 
-class fastNLOAlphas : public fastNLOLHAPDF {   
+class fastNLOAlphas : public fastNLOLHAPDF {
 
 public:
-   fastNLOAlphas(string name) : fastNLOLHAPDF(name) {
-      ;
-   };
-   fastNLOAlphas(string name, string LHAPDFFile, int PDFSet = 0) : fastNLOLHAPDF(name,LHAPDFFile,PDFSet), fAlphasMz(0.1184) {
-      ;
-   };
+   fastNLOAlphas(string name);
+   fastNLOAlphas(string name, string LHAPDFFile, int PDFSet);
 
    // ---- Alphas vars ---- //
    // Setters
@@ -57,9 +53,7 @@ public:
    void SetNLoop(int nloop);
    void SetAlphasMz(double AlphasMz , bool ReCalcCrossSection = false);
    // Getters
-   double GetAlphasMz() const {
-      return fAlphasMz;
-   };
+   double GetAlphasMz() const;
    void SetGRVtoPDG2012_2loop();
 
 
@@ -72,71 +66,6 @@ protected:
 
 };
 
-
-//______________________________________________________________________________
-void fastNLOAlphas::SetMz(double Mz) {
-   Alphas::SetMz(Mz);
-}
-
-void fastNLOAlphas::SetNFlavor(int nflavor) {
-   if (nflavor == 0) {
-      Alphas::SetFlavorMatchingOn(true);
-      Alphas::SetNf(6);
-      warn["SetNFlavor"]<<"GRV evolution of alpha_s is implemented for Nf=5 only.\n";
-      warn["SetNFlavor"]<<"You chose a variable Nf with Nfmax=6, i.e. results for Nf other than 5 presumably are wrong!\n";
-   } else if (nflavor == 5) {
-      Alphas::SetNf(nflavor);
-   } else {
-      error["SetNFlavor"]<<"GRV evolution of alpha_s is implemented for Nf=5 only.\n";
-      exit(1);
-   }
-}
-
-void fastNLOAlphas::SetNLoop(int nloop) {
-   Alphas::SetNLoop(nloop);
-}
-
-void fastNLOAlphas::SetAlphasMz(double AlphasMz , bool ReCalcCrossSection) {
-   debug["SetAlphasMz"]<<"Setting alpha_s(Mz)="<<AlphasMz<<" and RecalculateCrossSection="<<(ReCalcCrossSection?"Yes":"No")<<endl;
-   //
-   //  Set the alpha_s value at M_Z
-   //
-   fAlphasMz    = AlphasMz;             // new alpha_s value
-   if (ReCalcCrossSection) CalcCrossSection();
-}
-
-
-//______________________________________________________________________________
-
-
-double fastNLOAlphas::EvolveAlphas(double Q) const {
-   //
-   // Implementation of Alpha_s evolution as function of Mu_r only.
-   //
-   // the alpha_s evolution is done within LHAPDF.
-   //
-   // WARNING: You cannot change alpha_s(Mz), but is is
-   // defined with the pdf. 'alphasMz' is not used here!
-   //
-   return Alphas::CalcAlphasMu(Q , fAlphasMz);
-}
-
-
-//______________________________________________________________________________
-
-
-void fastNLOAlphas::SetGRVtoPDG2012_2loop() {
-   info["SetGrVtoPDF2012"]<<"Resetting to GRV Alphas::Alphas evolution."<<endl;
-   Alphas::SetMz(91.1876); // PDG 2012
-   Alphas::SetNf(5);
-   Alphas::SetNLoop(2);
-   Alphas::SetFlavorMatchingOn(false);
-   if (info.GetSpeak()) {
-      info<<"Calling Alphas::PrintInfo()."<<endl;
-      info<<"Alpha_s(Mz) value is taken from fastNLOAlphas, instead of Alphas::Alphas."<<endl;
-      Alphas::PrintInfo();
-   }
-}
 
 
 //______________________________________________________________________________
