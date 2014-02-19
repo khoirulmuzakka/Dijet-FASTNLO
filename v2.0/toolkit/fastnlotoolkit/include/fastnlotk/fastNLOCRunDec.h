df88c55a18561222a68fbfbf7dfc91f02c7ff6c5
Georg Sieber <georg.sieber@cern.ch>
2014-02-19 22:23:28 +0000

    splitted up fastNLOCrunDec and fastNLOQCDNUMAS
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1709 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/toolkit/fastnlotoolkit/include/fastnlotk/fastNLOCRunDec.h
+++ v2.0/toolkit/fastnlotoolkit/include/fastnlotk/fastNLOCRunDec.h
@@ -36,177 +36,46 @@
 #include <LHAPDF/LHAPDF.h>
 #include "speaker.h"
 #include "fastNLOLHAPDF.h"
-//#include "Alphas.h"
 #include "CRunDec.h"
 
 using namespace std;
 
 class fastNLOCRunDec : public fastNLOLHAPDF {
 
-   public:
-      fastNLOCRunDec(string name) : fastNLOLHAPDF(name) {
-         InitCRunDec();
-      };
-      fastNLOCRunDec(string name, string LHAPDFFile, int PDFSet = 0) : fastNLOLHAPDF(name,LHAPDFFile,PDFSet), fAlphasMz(0.1184) {
-         InitCRunDec();
-      };
+    public:
+        fastNLOCRunDec(string name);
+        fastNLOCRunDec(string name, string LHAPDFFile, int PDFSet);
+        // ---- Alphas vars ---- //
+        // Setters
+        void SetMz(double Mz);
+        void SetNFlavor(int nflavor);
+        void SetNLoop(int nloop);
+        void SetQMass(int pdgid, double qmass);
+        void SetAlphasMz(double AlphasMz , bool ReCalcCrossSection);
+        void SetLHAPDFValues();
+        void SetPDGValues();
+        // Getters
+        double GetMz() const;
+        double GetQMass(int pdgid) const;
+        int GetNFlavor() const;
+        int GetNLoop() const;
+        double GetAlphasMz() const;
 
-      // ---- Alphas vars ---- //
-      // Setters
-      void SetMz(double Mz);
-      void SetNFlavor(int nflavor);
-      void SetNLoop(int nloop);
-      void SetQMass(int pdgid, double qmass);
-      void SetAlphasMz(double AlphasMz , bool ReCalcCrossSection = false);
-      void SetLHAPDFValues();
-      void SetPDGValues();
-      // Getters
-      double GetMz() const {
-         return fMz;
-      }
-      double GetQMass(int pdgid) const {
-         return QMass[pdgid];
-      }
-      int GetNFlavor() const {
-         return fnFlavor;
-      }
-      int GetNLoop() const {
-         return fnLoop;
-      }
-      double GetAlphasMz() const {
-         return fAlphasMz;
-      };
+    protected:
 
+        // inherited functions
+        double EvolveAlphas(double Q) const ;
 
-
-   protected:
-
-   // inherited functions
-   double EvolveAlphas(double Q) const ;
-
-   // ---- Alphas vars ---- //
-   CRunDec *crundec;
-   void InitCRunDec();
-   double fAlphasMz;
-   double fMz;
-   int fnFlavor;
-   int fnLoop;
-   double QMass[6];
+        // ---- Alphas vars ---- //
+        CRunDec *crundec;
+        void InitCRunDec();
+        double fAlphasMz;
+        double fMz;
+        int fnFlavor;
+        int fnLoop;
+        double QMass[6];
 
 
 };
-
-
-//______________________________________________________________________________
-void fastNLOCRunDec::InitCRunDec() {
-
-   crundec = new CRunDec();
-   SetPDGValues();
-}
-
-void fastNLOCRunDec::SetPDGValues() {
-   // Initialize with PDG values
-   QMass[0]  = PDG_MD;
-   QMass[1]  = PDG_MU;
-   QMass[2]  = PDG_MS;
-   QMass[3]  = PDG_MC;
-   QMass[4]  = PDG_MB;
-   QMass[5]  = PDG_MT;
-   fMz       = PDG_MZ;
-   fAlphasMz = PDG_ASMZ;
-   //Variable flavor number scheme
-   fnFlavor = 0;
-   //2-loop alpha_s evolution
-   fnLoop = 2;
-}
-
-void fastNLOCRunDec::SetLHAPDFValues() {
-   FillPDFCache();
-   fAlphasMz = LHAPDF::alphasPDF(fMz);
-   fnLoop = LHAPDF::getOrderAlphaS() + 1;
-   for (int i = 0; i < 6; i++)
-      QMass[i] = LHAPDF::getQMass(i+1);
-}
-
-void fastNLOCRunDec::SetMz(double Mz) {
-   fMz = Mz;
-}
-
-void fastNLOCRunDec::SetNFlavor(int nflavor) {
-   fnFlavor = nflavor;
-}
-
-void fastNLOCRunDec::SetNLoop(int  nloop) {
-   fnLoop = nloop;
-}
-
-void fastNLOCRunDec::SetQMass(int pdgid, double qmass) {
-   QMass[pdgid] = qmass;
-}
-
-void fastNLOCRunDec::SetAlphasMz(double AlphasMz , bool ReCalcCrossSection) {
-   debug["SetAlphasMz"]<<"Setting alpha_s(Mz)="<<AlphasMz<<" and RecalculateCrossSection="<<(ReCalcCrossSection?"Yes":"No")<<endl;
-   //
-   //  Set the alpha_s value at M_Z
-   //
-   fAlphasMz    = AlphasMz;             // new alpha_s value
-   //if (ReCalcCrossSection) CalcCrossSection();
-
-}
-
-//______________________________________________________________________________
-double fastNLOCRunDec::EvolveAlphas(double Q) const {
-   //
-   // Implementation of Alpha_s evolution as function of Mu_r only.
-   //
-   // the alpha_s evolution is done within LHAPDF.
-   //
-   // WARNING: You cannot change alpha_s(Mz), but is is
-   // defined with the pdf. 'lphasMz' is not used here!
-   //
-   //return crundec.AlphasExact(fAlphasMz, Mz, Q, nflavor, nloop);
-   //
-   //FFNS
-   if (fnFlavor != 0) {
-      return crundec->AlphasExact(fAlphasMz, fMz, Q, fnFlavor, fnLoop);
-   }
-
-   //TODO: Replace with better code
-   //VFNS
-   //Always evolving from Mz as starting scale
-   //Crossing the mt threshold
-   if (Q > QMass[5]) {
-      crundec->nfMmu[0].nf = 6;
-      crundec->nfMmu[0].Mth = QMass[5];
-      crundec->nfMmu[0].muth = QMass[5];
-      return crundec->AlL2AlH(fAlphasMz, fMz, crundec->nfMmu, Q, fnLoop);
-   }
-   //Not Crossing any threshold
-   else if ( Q > QMass[4]) {
-      return crundec->AlphasExact(fAlphasMz, fMz, Q, 5, fnLoop);
-   }
-   //Crossing mb threshold
-   else if (Q > QMass[3]) {
-      crundec->nfMmu[0].nf = 5;
-      crundec->nfMmu[0].Mth = QMass[4];
-      crundec->nfMmu[0].muth = QMass[4];
-      return crundec->AlH2AlL(fAlphasMz, fMz, crundec->nfMmu, Q, fnLoop);
-   }
-   //Crossing mc and mb threshold
-   else {
-      crundec->nfMmu[0].nf = 5;
-      crundec->nfMmu[0].Mth = QMass[4];
-      crundec->nfMmu[0].muth = QMass[4];
-      crundec->nfMmu[1].nf = 4;
-      crundec->nfMmu[1].Mth = QMass[3];
-      crundec->nfMmu[1].muth = QMass[3];
-      return crundec->AlH2AlL(fAlphasMz, fMz, crundec->nfMmu, Q, fnLoop);
-   }
-}
-
-
-
-//______________________________________________________________________________
-
 
 #endif
--- v2.0/toolkit/fastnlotoolkit/include/fastnlotk/fastNLOQCDNUMAS.h
+++ v2.0/toolkit/fastnlotoolkit/include/fastnlotk/fastNLOQCDNUMAS.h
@@ -54,12 +54,8 @@ extern "C" {
 class fastNLOQCDNUMAS : public fastNLOLHAPDF {
 
 public:
-   fastNLOQCDNUMAS(string name) : fastNLOLHAPDF(name) {
-      SetPDGValues();
-   };
-   fastNLOQCDNUMAS(string name, string LHAPDFFile, int PDFSet = 0) : fastNLOLHAPDF(name,LHAPDFFile,PDFSet), fAlphasMz(0.1184) {
-      SetPDGValues();
-   };
+   fastNLOQCDNUMAS(string name);
+   fastNLOQCDNUMAS(string name, string LHAPDFFile, int PDFSet);
    //inherited
    void CalcCrossSection();
 
@@ -74,21 +70,11 @@ public:
    void SetPDGValues();
    void SetLHAPDFValues();
    // Getters
-   double GetMz() const {
-      return fMz;
-   }
-   double GetQMass(int pdgid) const {
-      return QMass[pdgid];
-   }
-   int GetNFlavor(int nflavor) const {
-      return nflavor;
-   }
-   int GetNLoop() const {
-      return fnLoop;
-   }
-   double GetAlphasMz() const {
-      return fAlphasMz;
-   };
+   double GetMz() const;
+   double GetQMass(int pdgid) const;
+   int GetNFlavor(int nflavor) const;
+   int GetNLoop() const;
+   double GetAlphasMz() const;
 
 
 
@@ -105,126 +91,5 @@ protected:
 
 
 };
-
-
-//______________________________________________________________________________
-//
-void fastNLOQCDNUMAS::SetMz(double Mz) {
-   fMz = Mz;
-}
-
-void fastNLOQCDNUMAS::SetNFlavor(int  nflavor) {
-   fnFlavor = nflavor;
-}
-
-void fastNLOQCDNUMAS::SetNLoop(int  nloop) {
-   fnLoop = nloop;
-}
-
-void fastNLOQCDNUMAS::SetQMass(int pdgid, double qmass) {
-   QMass[pdgid] = qmass;
-}
-
-void fastNLOQCDNUMAS::SetAlphasMz(double AlphasMz , bool ReCalcCrossSection) {
-   debug["SetAlphasMz"]<<"Setting alpha_s(Mz)="<<AlphasMz<<" and RecalculateCrossSection="<<(ReCalcCrossSection?"Yes":"No")<<endl;
-   //
-   //  Set the alpha_s value at M_Z
-   //
-   fAlphasMz    = AlphasMz;             // new alpha_s value
-   if (ReCalcCrossSection) CalcCrossSection();
-
-}
-
-void fastNLOQCDNUMAS::SetPDGValues() {
-   // Initialize with PDG values
-   QMass[0]  = PDG_MD;
-   QMass[1]  = PDG_MU;
-   QMass[2]  = PDG_MS;
-   QMass[3]  = PDG_MC;
-   QMass[4]  = PDG_MB;
-   QMass[5]  = PDG_MT;
-   fMz       = PDG_MZ;
-   fAlphasMz = PDG_ASMZ;
-   //Variable flavor number scheme
-   fnFlavor = 0;
-   //2-loop alpha_s evolution
-   fnLoop = 2;
-}
-
-void fastNLOQCDNUMAS::SetLHAPDFValues() {
-   FillPDFCache();
-   fAlphasMz = LHAPDF::alphasPDF(fMz);
-   fnLoop = LHAPDF::getOrderAlphaS() + 1;
-   for (int i = 0; i < 6; i++)
-      QMass[i] = LHAPDF::getQMass(i+1);
-}
-
-void fastNLOQCDNUMAS::InitEvolveAlphas() {
-   //Ensure reasonable values are set
-   //TODO Really neccessary?
-   char filename[] = " ";
-   int len_filename = strlen(filename);
-   int lun = 6;
-   qcinit_(&lun, filename, len_filename);
-
-
-   //LHAPDF LO=0 while QCDNUM LO=1
-   int iord = fnLoop;
-
-   //TODO Set correct Array in q2. maybe fnloreader. getQScale...
-   double qarr[2] = {1.0, 1000000};
-   double wgt[2] =  {1.0, 1.0};
-   //Length of array
-   int n= 2;
-   //Number of grid points
-   int nqin = 140;
-   //Real number generated grid points
-   int nqout = 0;
-   //Create Q2 Grid
-   gqmake_(qarr, wgt, &n, &nqin, &nqout);
-   setord_(&iord);
-   double r2 = fMz * fMz;
-   setalf_(&fAlphasMz, &r2);
-   //Get Indices of Flavor Thresholds (currently just the Q mass)
-   double Q2Mass[6];
-   for (int i = 0; i < 6; i++)
-      Q2Mass[i] = QMass[i]*QMass[i];
-
-   int iqc = iqfrmq_(&Q2Mass[3]) ;
-   int iqb = iqfrmq_(&Q2Mass[4]);
-   int iqt = iqfrmq_(&Q2Mass[5]);
-
-   //cout << iqc << " " << iqb << " " << iqt << endl;
-   //When fNFlavor = 0 VFNS if >0 then FFNS
-   //iqc,b,t are neglected if fnflavor =0
-   setcbt_(&fnFlavor, &iqc, &iqb, &iqt);
-
-}
-
-
-//______________________________________________________________________________
-double fastNLOQCDNUMAS::EvolveAlphas(double Q) const {
-   //
-   // Implementation of Alpha_s evolution as function of Mu_r only.
-   //
-   double mu2 = Q*Q;
-   int ierr = 9876;
-   //Number of really used flavors
-   int nf = 9;
-   double as = asfunc_(&mu2, &nf , &ierr);
-   //cout << as << "  " << mu2 << " " << nf << endl;
-   if (ierr > 0)
-      error["EvolveAlphas"]<<"Alphas evolution failed. ierr = "<<ierr<<", Q = "<<Q<<endl;
-   return as;
-
-}
-
-void fastNLOQCDNUMAS::CalcCrossSection() {
-   InitEvolveAlphas();
-   fastNLOLHAPDF::CalcCrossSection();
-}
-
-//______________________________________________________________________________
-
 
 #endif
