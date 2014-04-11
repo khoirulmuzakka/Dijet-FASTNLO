6ee53717ac8497f2f46469c7a0476ee901b8adcb
Daniel Britzger <daniel.britzger@desy.de>
2014-04-11 14:06:00 +0000

    Modify fnlo-tk-example
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1728 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/toolkit/src/fnlo-tk-example.cc
+++ v2.0/toolkit/src/fnlo-tk-example.cc
@@ -1,6 +1,6 @@
 //********************************************************************
 //
-//     fastNLO
+//     fnlo-tk-example
 //     This is your playgroud to calculate cross sections
 //     with fastNLO.
 //
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include "fastnlotk/fastNLOLHAPDF.h"
@@ -22,7 +23,7 @@ int main(int argc, char** argv) {
 
    // ---  Parse commmand line
    if (argc <= 1) {
-      cout<<" Program fastNLO"<<endl;
+      cout<<" Program fnlo-tk-example"<<endl;
       cout<<"   usage:"<<endl;
       cout<<"   fastNLO <fastNLO-table.tab> [<LHAPDF-file>]"<<endl;
       return 0;
@@ -34,8 +35,8 @@ int main(int argc, char** argv) {
    if (argc > 2)    PDFFile = (const char*) argv[2];
 
    //--- give some output
-   cout<<" fastNLO: Evaluating table: " << tablename << endl;
-   cout<<" fastNLO: Using PDF set   : " << PDFFile << endl;
+   cout<<" fnlo-tk-example: Evaluating table: " << tablename << endl;
+   cout<<" fnlo-tk-example: Using PDF set   : " << PDFFile << endl;
 
 
    // --- this is your playgroud to use fastNLO
@@ -44,18 +45,59 @@ int main(int argc, char** argv) {
    //    './src/fnlo-cppread.cc'
 
    //--- example calculation
-   fastNLOLHAPDF fnlo(tablename,PDFFile,0);
-   fnlo.PrintFastNLOTableConstants();		// print even more information
-   fnlo.PrintTableInfo();			// print some valuable information
-   fnlo.SetContributionON(fastNLO::kFixedOrder,0,true); // switch contributions on/off. By default LO and NLO.
-   fnlo.SetContributionON(fastNLO::kFixedOrder,1,true);
-   fnlo.SetContributionON(fastNLO::kFixedOrder,2,false); // NNLO must be switched on explicitly
-   fnlo.SetExternalFuncForMuR( &Function_Mu );	// set function to calculate renormalization scale. See manual for default settings.
-   fnlo.SetExternalFuncForMuF( &Function_Mu );
+   fastNLOLHAPDF fnlo(tablename,PDFFile,0);     // initialize a fastNLO instance with interface to LHAPDF.
+   //fnlo.PrintFastNLOTableConstants();		// print even more information
+   //fnlo.PrintTableInfo();			// print some valuable information
+   //fnlo.SetUnits(kAbsoluteUnits);		// Use units as specified in the publication or in barns.
+   //fnlo.SetContributionON(fastNLO::kFixedOrder,0,true); // switch contributions on/off. By default LO and NLO.
+   //fnlo.SetContributionON(fastNLO::kFixedOrder,1,true);
+   //fnlo.SetContributionON(fastNLO::kFixedOrder,2,true); // NNLO must be switched on explicitly
+   fnlo.CalcCrossSection();			// Calculate the cross section
+   fnlo.PrintCrossSections();			// Print cross section to screen
+
+
+   vector<double> cs = fnlo.GetCrossSection();	// Access cross sections for later usage
+
+   // finish:
+   return 0;
+
+
+   // example code how to loop over all PDF eigenvectors
+   cout<<"\n fnlo-tk-example: Now we want to loop over the eigenvectors of "<<PDFFile<<"."<<endl<<endl;
+   fnlo.SetLHAPDFFilename(PDFFile); // we use again the 'nominal' PDF-file
+   int nEig = fnlo.GetNPDFMembers(); // How many eigenvectors are there?
+   cout<<" fnlo-tk-example: There are "<<nEig<<" Eigenvalue sets in "<<PDFFile<<endl;
+   for ( int i = 0 ; i<nEig ; i++ ) { // start with 0
+      cout<<" fnlo-tk-example: Setting PDF member: "<<i<<" ***"<<endl;
+      fnlo.SetLHAPDFMember(i);	// specify the PDF member
+      fnlo.CalcCrossSection();  // redo cross section calculation
+      fnlo.PrintCrossSections(); // print new cross sections to screen
+      // write tot file
+      vector<double> cs = fnlo.GetCrossSection(); // get cross setions for further usage (i.e. printing to file)
+   }
+
+   // finish
+   return 0;
+
+   // example to use different scale factors
+   cout<<"\n fnlo-tk-example: Now we use a scale factor of 2."<<endl;
+   fnlo.SetScaleFactorsMuRMuF(2.0,2.0);
    fnlo.CalcCrossSection();
    fnlo.PrintCrossSections();
-   fnlo.PrintCrossSectionsDefault();
-   vector<double > xs = fnlo.GetCrossSection();
+   
+
+   cout<<"\n fnlo-tk-example: Now we use a scale factor of 0.5."<<endl;
+   fnlo.SetScaleFactorsMuRMuF(0.5,0.5);
+   fnlo.CalcCrossSection();
+   fnlo.PrintCrossSections();
+   
+
+   cout<<"\n fnlo-tk-example: Now we go back to the nominal result: 1."<<endl;
+   fnlo.SetScaleFactorsMuRMuF(1.0,1.0);
+   fnlo.CalcCrossSection();
+   fnlo.PrintCrossSections();
+   
+   // finish
    return 0;
 
 }
