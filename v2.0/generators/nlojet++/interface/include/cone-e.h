35bfb32fb3c39b276759b78fd0f80a55da17bca9
Klaus Rabbertz <klaus.rabbertz@cern.ch>
2014-05-19 11:01:26 +0000

    Still not working test version
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1761 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

---
+++ v2.0/generators/nlojet++/interface/include/cone-e.h
@@ -0,0 +1,33 @@
+#ifndef __cone_e_h__
+#define __cone_e_h__ 1
+
+
+#include <bits/hhc-event.h>
+#include <bits/hep-bounded_vector.h>
+
+using namespace std;
+using namespace nlo;
+
+
+class cone_e
+{
+  //   private types
+  typedef lorentzvector<double> _Lv;
+  
+ public:
+  //   do the clustering and return with the momenta of the jets
+  const bounded_vector<_Lv>& operator()(const event_hhc&, double);
+  
+ private:
+  //   private data members
+  bounded_vector<_Lv> _M_p;
+  bounded_vector<_Lv> _M_pj;
+  bounded_vector<_Lv> _M_ax;
+  
+  //   private members
+  double _M_pair(int, int, double);
+  void _M_merge(int i, int j) { _M_p[i] += _M_p[j];}
+};
+
+
+#endif
