03ca261a1bd45632fd227337ff1714644f546ab8
styros <styros>
2014-09-02 15:23:24 +0000

    Made it pretty
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@1890 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/toolkit/src/fnlo-tk-yodaout.cc
+++ v2.0/toolkit/src/fnlo-tk-yodaout.cc
@@ -23,11 +23,13 @@ double Function_Mu(double s1, double s2);
 //   -----------------------------------------------------
 
 //______________________________________________________________________________________________________________
+
 int main(int argc, char** argv) {
    // usage: fastNLO <fastNLO-table.tab> [<LHAPDFfile>]
    using namespace std;
-   using namespace say;          // namespace for 'speaker.h'-verbosity levels
-   using namespace fastNLO;      // namespace for fastNLO constants
+   using namespace say;          									// namespace for 'speaker.h'-verbosity levels
+   using namespace fastNLO;      									// namespace for fastNLO constants
+
 
    // ---  Parse commmand line
    if (argc <= 1) {
@@ -36,25 +38,24 @@ int main(int argc, char** argv) {
       cout<<"   fastNLO <fastNLO-table.tab> [<LHAPDF-file>]"<<endl;
       return 0;
    }
+
+
    // --- fastNLO table
    string tablename =  (const char*) argv[1];
    //---  PDF set
    string PDFFile = "CT10nlo.LHgrid";
    if (argc > 2)    PDFFile = (const char*) argv[2];
 
-   //--- give some output
+
+   // --- give some output
    cout<<" fnlo-tk-yodaout: Evaluating table: " << tablename << endl;
    cout<<" fnlo-tk-yodaout: Using PDF set   : " << PDFFile << endl;
 
 
-
-
-   // First we find the point of change of the rapidity value, then for each one we create a histogram which we store in a vector and
-   // in the end save it in a yoda file
-
+   // --- getting the scale factors from input in form xmur,xmuf
    float xmur = 1.0;
    float xmuf = 1.0;
-   if (argc > 3){                           //getting the scale factors from input in form xmur,xmuf
+   if (argc > 3){                          								 
       string scalefactors = argv[3];
       xmur = (float)::atof(scalefactors.substr( 0, scalefactors.find(",") ).c_str());
       xmuf = (float)::atof(scalefactors.substr( scalefactors.find(",")+1, scalefactors.size()-scalefactors.find(",")-1 ).c_str());
@@ -62,30 +63,24 @@ int main(int argc, char** argv) {
    cout << " fnlo-tk-yodaout: Renormalization and factorization scale factors: " << xmur << " and " << xmuf << "\n";
 
 
-
-   fastNLOLHAPDF fnlo(tablename,PDFFile,0);                    // initialize a fastNLO instance with interface to LHAPDF.
-   fnlo.PrintTableInfo();                                       // print some valuable information
-   if ( xmur != 1.0 || xmuf != 1.0 ){ fnlo.SetScaleFactorsMuRMuF(xmur,xmuf); }     //set the desired scale factors (1.0 is default)
-   fnlo.CalcCrossSection();                                    // Calculate the cross section
-   //fnlo.PrintCrossSections();                                // Print cross section to screen
-
-
-   std::vector<YODA::HistoBin1D> bins;                                      // vector that will accept the pT bins
-   YODA::Writer & writer = YODA::WriterYODA::create();                        // creat the writer for the yoda file
-   std::vector< YODA::AnalysisObject * > ao;                                  // vector that will accept the pointers of the histograms for each rapidity value
+   // --- fastNLO initialisation
+   fastNLOLHAPDF fnlo(tablename,PDFFile,0);                    						// initialise a fastNLO instance with interface to LHAPDF.
+   fnlo.PrintTableInfo();                                       					// print some valuable information
+   if ( xmur != 1.0 || xmuf != 1.0 ){ fnlo.SetScaleFactorsMuRMuF(xmur,xmuf); }     			//set the desired scale factors (1.0 is default)
+   fnlo.CalcCrossSection();                                    						// Calculate the cross section
+   //fnlo.PrintCrossSections();                                						// Print cross section to screen
 
 
-   //Get RivetID and running number in histogram name by spotting the capital letter in "RIVETID=" in the fnlotable
-   size_t capital_pos = 0;                           //RivetId capital letter where the histogram counter runs
+   // --- Get RivetID and running number in histogram name by spotting the capital letter in "RIVETID=" in the fnlotable
+   size_t capital_pos = 0;                           							//RivetId capital letter where the histogram counter runs
    string RivetId = fnlo.GetRivetId();
    if (RivetId.empty()) {
       error["fnlo-tk-yodaout"] << "No Rivet ID found in fastNLO Table." << endl;
       exit(1);
    }
-
    for (size_t i = fnlo.GetRivetId().find("/"); i < fnlo.GetRivetId().size(); i++){
-      if (isupper(fnlo.GetRivetId()[i])){                  //find capital letter
-         RivetId[i] = tolower(RivetId[i]);               //and lower it
+      if (isupper(fnlo.GetRivetId()[i])){                  						//find capital letter
+         RivetId[i] = tolower(RivetId[i]);               						//and lower it
          capital_pos = i;
          break;
       }
@@ -95,7 +90,8 @@ int main(int argc, char** argv) {
       exit(1);
    }
 
-   //naming the file with PDF and scaling factors
+
+   // --- naming the file with PDF and scaling factors
    string PDFName = PDFFile.substr(0, PDFFile.size() - 7);
    stringstream Xmur, Xmuf;
    Xmur << xmur;
@@ -103,32 +99,36 @@ int main(int argc, char** argv) {
    string FileName = PDFName + "_" + Xmur.str() + "_" + Xmuf.str();
 
 
+   // --- histogram creation and storage
+   std::vector<YODA::HistoBin1D> bins;                                                                  		// vector that will accept the pT bins
+   YODA::Writer & writer = YODA::WriterYODA::create();                                                  		// creat the writer for the yoda file
+   std::vector< YODA::AnalysisObject * > ao;                                                            		// vector of pointers of histograms for each rapidity
 
-   //histogram creation and storage
    size_t offset = atoi(RivetId.substr(capital_pos +1, 2).c_str());
-   for (size_t i=0; i < fnlo.GetNBinDimI(); i++) {                                        // for all rapidity bins
-      std::vector<YODA::HistoBin1D> bins;                                               // vector that will accept the pT bins
+   for (size_t i=0; i < fnlo.GetNBinDimI(); i++) {                                        				// for all rapidity bins
+      std::vector<YODA::HistoBin1D> bins;                                               				// vector that will accept the pT bins
 
-      stringstream histno;                                                              // just to make i+1 from int
-      histno << offset+i;                                                                    // to a string for the naming
+      stringstream histno;                                                              				// just to make i+1 from int
+      histno << offset+i;                                                                    				// to a string for the naming
 
-      RivetId.replace( capital_pos +3 - histno.str().size(), histno.str().size(), histno.str());         // next histogram name
+      RivetId.replace( capital_pos +3 - histno.str().size(), histno.str().size(), histno.str());         		// next histogram name
 
-      for (int k = 0 ; k< fnlo.GetNBinDimII(i) ; k++) {                                  // starting from the first pT bin of each rapidity
-         bins.push_back(YODA::HistoBin1D( fnlo.GetBinDimII(i)[k].first , fnlo.GetBinDimII(i)[k].second ) );       // insert pT bin into the vector
+      for (int k = 0 ; k< fnlo.GetNBinDimII(i) ; k++) {                                  				// starting from the first pT bin of each rapidity
+         bins.push_back(YODA::HistoBin1D( fnlo.GetBinDimII(i)[k].first , fnlo.GetBinDimII(i)[k].second ) );       	// insert pT bin into the vector
       }
 
-      YODA::Histo1D * hist = new YODA::Histo1D(bins, "/" + RivetId, FileName   );               //create histogram pointer
+      YODA::Histo1D * hist = new YODA::Histo1D(bins, "/" + RivetId, FileName   );               			//create histogram pointer
       // pointer in order not to be deleted after we exit the loop, so we can then save them into the yoda file
 
-      for (int k =0 ; k< fnlo.GetNBinDimII(i) ; k++) {                        // fill in the histogram (*length as we fill area)
+      for (int k =0 ; k< fnlo.GetNBinDimII(i) ; k++) {                        						// fill in the histogram (*length as we fill area)
          hist->fill( (fnlo.GetBinDimII(i)[k].first + fnlo.GetBinDimII(i)[k].second)/2.0 ,
                fnlo.GetCrossSection2Dim()[i][k]*(fnlo.GetBinDimII(i)[k].second - fnlo.GetBinDimII(i)[k].first) );
       }
-      ao.push_back(hist);                                                               // insert the histogram pointer into the vector
+      ao.push_back(hist);                                                               				// insert the histogram pointer into the vector
    }
 
-   writer.write( FileName + ".yoda", ao );                                                  // save histograms into the yoda file
+   // --- output
+   writer.write( FileName + ".yoda", ao );                                                 				// save histograms into the yoda file
    cout << "###################################################################################" << "\n \n";
    cout << FileName + ".yoda was succesfully produced" << "\n \n";
 
