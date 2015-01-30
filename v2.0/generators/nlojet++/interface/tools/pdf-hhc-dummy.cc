95f014768efae10a93bb19fa81a95faedb5aab58
Daniel Britzger <daniel.britzger@desy.de>
2015-01-30 10:53:29 +0000

    remove redundant for loop
    git-svn-id: https://ekptrac.physik.uni-karlsruhe.de/svn/fastNLO/trunk@2005 aa5437bd-d6fa-0310-9a40-cbaab9bf9339

--- v2.0/generators/nlojet++/interface/tools/pdf-hhc-dummy.cc
+++ v2.0/generators/nlojet++/interface/tools/pdf-hhc-dummy.cc
@@ -15,11 +15,8 @@ weight_hhc pdf_hhc_dummy::pdf(double x1, double x2, double mf2, unsigned int nu,
   retval[5] = 1. / x1 / x2;
   retval[6] = 1. / x1 / x2;
 
-  for (int i = 0; i < 7; i++) {
-    if (isnan(retval[i])) {
-      cout << "fastNLO.pdf_hhc_dummy: WARNING! NaN for 1/x1/x2 in no. " << i
-           << ", x1 = " << x1 << ", x2 = " << x2 << endl;
-    }
+  if (isnan(retval[0])) {
+     cout << "fastNLO.pdf_hhc_dummy: WARNING! NaN for pdf = 1/x1/x2 with x1 = " << x1 << ", x2 = " << x2 << endl;
   }
   return retval;
 }
